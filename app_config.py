192d7aa7827b97e790c63c9c82ad073d16caba3e
Alyson Hurt <ahurt@npr.org>
2013-05-23 23:33:12 -0400

    updated project_name in app config

--- app_config.py
+++ app_config.py
@@ -14,7 +14,7 @@ import os
 NAMES
 """
 # Project name used for display
-PROJECT_NAME = 'breaking-news-facts'
+PROJECT_NAME = 'Serendipity Day: Breaking News Facts'
 
 # Project name used for paths on the filesystem and in urls
 # Use dashes, not underscores
