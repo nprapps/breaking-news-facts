b94bdd807c7724f6ed5332d47ae2b4200a406f91
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-10-10 11:58:29 -0400

    Data loader working

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from peewee import *
 
 from app_config import get_secrets
@@ -9,7 +11,28 @@ psql_db = PostgresqlDatabase('breaking',
     password=secrets['APPS_PASS']
 )
 
+
+def delete_tables():
+    try:
+        Event.drop_table()
+    except:
+        pass
+
+    try:
+        Fact.drop_table()
+    except:
+        pass
+
+
+def create_tables():
+    Event.create_table()
+    Fact.create_table()
+
+
 class Event(Model):
+    """
+    An event with a series of facts.
+    """
     name = CharField()
     start_date = DateField()
 
@@ -20,8 +43,14 @@ class Event(Model):
     def get_admin_url(self):
         return '/admin/events/%s/' % self.id
 
+    def __unicode__(self):
+        return self.name
+
 
 class Fact(Model):
+    """
+    An instance of a fact. Related to a master fact.
+    """
     statement = TextField()
     attribution = TextField()
     timestamp = DateTimeField()
@@ -29,10 +58,16 @@ class Fact(Model):
     # 0 - Has been confirmed as false.
     # 1 - Has been confirmed as true.
     # 2 - Neither confirmed nor denied nor checking.
-    # 3 - Possibly true, checking.
+    # 3 - Checking.
     status = IntegerField(default=2)
-    related_facts = ForeignKeyField('self', related_name='related_facts', null=True)
+    related_facts = ForeignKeyField('self', null=True)
+    public = BooleanField(default=False)
+    approved = BooleanField(default=False)
+    reporter = CharField()
 
     class Meta:
         database = psql_db
         db_table = 'facts'
+
+    def __unicode__(self):
+        return self.statement
